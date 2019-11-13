0e5ef8462c21da6e40bc8be37f5e81e427cc1044
PaperPlane01 <dagfortnewage@gmail.com>
2019-11-13 19:00:02 +0600

    feat: (13.11.19) Moved version info from right to left

--- src/Footer/components/Footer.tsx
+++ src/Footer/components/Footer.tsx
@@ -5,12 +5,10 @@ const {version} = require("../../../package.json");
 
 export const Footer: React.FC<{}> = () => (
     <StickyFooter stickyStyles={{width: '100%'}}>
-        <div style={{float: 'right'}}>
-            <Typography variant="caption"
-                        color="textSecondary"
-            >
-                Service node client v. {version}
-            </Typography>
-        </div>
+        <Typography variant="caption"
+                    color="textSecondary"
+        >
+            Service node client v. {version}
+        </Typography>
     </StickyFooter>
 );
