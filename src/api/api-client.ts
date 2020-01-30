05ba4f78d3288cd9587a7ea0ca58ae92aeb64018
PaperPlane01 <dagfortnewage@gmail.com>
2020-01-30 15:56:32 +0600

    fix: (30.01.20) Fixed service node API base URL was not taken from .env file

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
 export const axiosInstance = Axios.create({
-    baseURL: /*process.env.REACT_APP_SERVICE_NODE_API_BASE_URL as string ||*/ "http://192.168.0.156:3002"
+    baseURL: process.env.REACT_APP_SERVICE_NODE_API_BASE_URL as string
 });
 
