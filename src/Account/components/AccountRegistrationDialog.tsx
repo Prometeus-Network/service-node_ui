ecbe190da2507cd1a1f22cb0d02371bf77619ade
PaperPlane01 <dagfortnewage@gmail.com>
2020-01-30 14:44:15 +0600

    build: (30.01.20) Migrated from NPM to Yarn

---
+++ src/Account/components/AccountRegistrationDialog.tsx
@@ -0,0 +1,97 @@
+import React, {FunctionComponent} from "react";
+import {observer, inject} from "mobx-react";
+import {Dialog, DialogTitle, DialogContent, DialogActions, Typography, Button, CircularProgress, TextField} from "@material-ui/core";
+import withMobileDialog, {WithMobileDialog} from "@material-ui/core/withMobileDialog";
+import {withSnackbar, WithSnackbarProps} from "notistack";
+import {RegisterAccountRequest} from "../../models";
+import {FormErrors} from "../../utils";
+import {ApiError} from "../../api";
+import {IAppState} from "../../store";
+
+interface AccountRegistrationDialogMobxProps {
+    registrationForm: Partial<RegisterAccountRequest>,
+    formErrors: FormErrors<RegisterAccountRequest>,
+    submissionError?: ApiError,
+    pending: boolean,
+    registrationDialogOpen: boolean,
+    showSnackbar: boolean,
+    setShowSnackbar: (showSnackbar: boolean) => void,
+    setFormValue: (key: keyof RegisterAccountRequest, value: string),
+    setRegistrationDialogOpen: (registrationDialogOpen: boolean) => void,
+    registerAccount: () => void
+}
+
+type AccountRegistrationDialogInjectedProps = WithMobileDialog & WithSnackbarProps;
+
+type AccountRegistrationDialog = AccountRegistrationDialogMobxProps & AccountRegistrationDialogInjectedProps;
+
+const _AccountRegistrationDialog: FunctionComponent<AccountRegistrationDialogProps> = ({
+                                                                                           registrationForm,
+                                                                                           formErrors,
+                                                                                           submissionError,
+                                                                                           pending,
+                                                                                           registrationDialogOpen,
+                                                                                           showSnackbar,
+                                                                                           setFormValue,
+                                                                                           setRegistrationDialogOpen,
+                                                                                           setShowSnackbar,
+                                                                                           registerAccount,
+                                                                                           fullScreen,
+                                                                                           enqueueSnackbar
+                                                                                       }) => {
+    if (showSnackbar) {
+        enqueueSnackbar("Account has been successfully registered");
+        setShowSnackbar(false);
+        setRegistrationDialogOpen(false);
+    }
+
+    return (
+        <Dialog open={registrationDialogOpen}
+                fullScreen={fullScreen}
+                fullWidth
+                maxWidth="md"
+                onClose={() => setRegistrationDialogOpen(false)}
+        >
+            <DialogTitle>Add wallet</DialogTitle>
+            <DialogContent>
+                <TextField label="Address"
+                           value={registrationForm.address}
+                           onChange={event => setFormValue("address", event.target.value)}
+                           fullWidth
+                           margin="dense"
+                           error={Boolean(formErrors.address)}
+                           helperText={formErrors.address && formErrors.address}
+                />
+                <TextField label="Private key"
+                           value={registrationForm.privateKey}
+                           onChange={event => setFormValue("privateKey", event.target.value)}
+                           fullWidth
+                           margin="dense"
+                           error={Boolean(formErrors.privateKey)}
+                           helperText={formErrors.privateKey && formErrors.privateKey}
+                           multiline
+                />
+                {submissionError && (
+                    <Typography variant="body1" style={{color: "red"}}>
+                    </Typography>
+                )}
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined"
+                        color="secondary"
+                        onClick={() => setRegistrationDialogOpen(false)}
+                >
+                    Cancel
+                </Button>
+                <Button variant="contained"
+                        color="primary"
+                        onClick={registerAccount}
+                        disabled={pending}
+                >
+                    Register
+                </Button>
+                {pending && <CircularProgress color="primary" size={15}/>}
+            </DialogActions>
+        </Dialog>
+    )
+};
